d23a8c0d2fa6a976cfef9e0e448f45fec006027a
Dan Seyfullin <dseyfullin@evolenthealth.com>
2020-02-14 22:49:16 -0500

    removed auto-generated overrides

--- Giraffe/Program.cs
+++ Giraffe/Program.cs
@@ -39,20 +39,6 @@ namespace Giraffe
 
         }
 
-        public override bool Equals(object obj)
-        {
-            return base.Equals(obj);
-        }
-
-        public override int GetHashCode()
-        {
-            return base.GetHashCode();
-        }
-
-        public override string ToString()
-        {
-            return base.ToString();
-        }
     }
 }
 
